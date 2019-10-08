61a0afe6b25f36e9201a6d59c3c8bae2d300e3fa
Krithy <krithy123@gmail.com>
2019-10-08 11:26:03 -0700

    this is my first commit

---
+++ src/main/java/com/companyname/project/SampleCalc.java
@@ -0,0 +1,19 @@
+package com.companyname.project;
+
+public class SampleCalc 
+{
+	    public long add(long first, long second) {
+	        return first + second;
+	    }
+	    public long subtract(long first, long second) {
+	        return first - second;
+	    }
+	    public long multiply(long first, long second) {
+	        return first * second;
+	    }
+	    public long divide(long first, long second) {
+	        return first / second;
+	    }
+}
+
+
